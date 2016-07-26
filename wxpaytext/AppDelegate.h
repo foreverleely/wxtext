dde57bede12dc546ab43abc2414b9302edc46d95
李阳 <1586557814@qq.com>
2016-07-26 15:16:50 +0800

    1

---
+++ wxpaytext/AppDelegate.h
@@ -0,0 +1,17 @@
+//
+//  AppDelegate.h
+//  wxpaytext
+//
+//  Created by liyangly on 16/7/26.
+//  Copyright © 2016年 liyangly. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface AppDelegate : UIResponder <UIApplicationDelegate>
+
+@property (strong, nonatomic) UIWindow *window;
+
+
+@end
+
